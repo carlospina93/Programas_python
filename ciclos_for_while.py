274f7a6be92458d129aef8511d97d53ea15babb7
carlos piña <carlos_pina93@hotmail.com>
2020-05-09 00:36:49 -0500

    ejercicio python ciclos

---
+++ ciclos_for_while.py
@@ -0,0 +1,12 @@
+## ciclos y loops
+## for se repite el numero de veces que tu indiques, para cuando tu sepas cuantas veces se va a repetir una accion
+## while se puede repetir cuantas veces quieras, para cuando no se sepa cuantas veces se va a repetir una accion
+
+##for i= 0; i<=10; i++
+for i in range(10,105,5):
+    print(i)
+    
+palabra = "clara Oswald"
+
+for i in range(palabra):
+    print(str(i))
---
+++ ejemplo_for.py
@@ -0,0 +1,13 @@
+for i in("Clara Oswald"):
+    print(i)
+
+array =["carlos","luis", "Nava"]
+
+for i in(array):
+    print(i)
+
+for i in range(0,11):
+    print(i)
+    if(i==5):
+        print("leego a 5")
+        continue
